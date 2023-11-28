fdc9a46e7c50bcde77a0c582f801fb3d371ebc1d
Oleksandr Nechyporenko <a.nechipor@crnd.int>
2023-11-28 18:10:52 +0200

    Multiple domains without operator

--- domain_to_expression.py
+++ domain_to_expression.py
@@ -9,10 +9,15 @@ OPERATORS_MAPPING = {
     '=': '==',
     'in': 'in',
     'not in': 'not in',
+    '|': ' or ',
+    '&': ' and ',
 }
+DONE_CONVERT = 0
+MISSED_CONVERT = 0
 
 
 def find_and_replace_attrs(xml_file_path):
+    global DONE_CONVERT
     try:
         tree = ET.parse(xml_file_path)
         root = tree.getroot()
@@ -30,6 +35,7 @@ def find_and_replace_attrs(xml_file_path):
                 del elem.attrib['attrs']
         # Save modified attrs
         tree.write(xml_file_path, encoding='utf-8', xml_declaration=True)
+        DONE_CONVERT += 1
     except ET.ParseError:
         _logger.error("Error parse file %s", xml_file_path)
 
@@ -52,18 +58,26 @@ def parse_attributes(attributes, xml_file_path):
 
 
 def get_expression(domain, xml_file_path):
-    if len(domain) == 1:
-        expression = ""
-        for expr in domain:
-            expression += '%s %s %s' % (
-                expr[0],
-                OPERATORS_MAPPING.get(expr[1]),
-                repr(expr[2]))
+    global MISSED_CONVERT
+    if len(domain) == 1 and isinstance(domain[0], tuple):
+        expression = '%s %s %s' % (
+            domain[0][0],
+            OPERATORS_MAPPING.get(domain[0][1]),
+            repr(domain[0][2]))
         print(domain, '->>>>>', expression)
         return expression
-    print('There is multiple domain %s in path %s. Passed.' % (
-        domain, xml_file_path))
-    return False
+    else:
+        operator = ' and '
+        if all(isinstance(item, tuple) for item in domain):
+            expr = [get_expression([expr], xml_file_path) for expr in domain]
+            expression = operator.join(expr)
+            print(domain, '->>>>>', expression)
+            return expression
+        else:
+            print('There is multiple domain %s in path %s. Passed.' % (
+                domain, xml_file_path))
+            MISSED_CONVERT += 1
+            return False
 
 
 def search_in_folders(root_folder):
@@ -75,3 +89,5 @@ def search_in_folders(root_folder):
 
 
 search_in_folders('%s/crnd-inc/generic-addons' % os.path.dirname(os.getcwd()))
+print('Done converted', DONE_CONVERT)
+print('Missed converted', MISSED_CONVERT)
